2a2e0e99108b38b3e70de48736bca860cc028556
Andrew Kachnic <ajkachnic@protonmail.com>
2020-04-27 11:12:25 -0400

    fix: Limit body size
    The body size could theoretically be infinite, so I made a few functions that limited the max character count to 500 characters.
    
    Resolves: #7

--- api/v1/new-stack.js
+++ api/v1/new-stack.js
@@ -1,19 +1,46 @@
 import nextConnect from "next-connect";
+import bodyParser from "body-parser";
+
 import middleware from "../../middleware/middleware";
 import limiter from "../../middleware/limiter";
-import validate from '@ajkachnic/validata';
 
-import { scrubURL } from '../../utils/validate';
+import { fitsMax, isUrl } from "@ajkachnic/validata";
+
+import { scrubURL } from "../../utils/validate";
 
 const handler = nextConnect();
 
+middleware.use(
+  bodyParser.json({
+    limit: ".5kb",
+  })
+);
 handler.use(
   limiter({
     max: 10,
-    expireTime: 1000 * 60 * 5 // 5 minutes
+    expireTime: 1000 * 60 * 5, // 5 minutes
   })
 );
+
 handler.use(middleware);
+
+function bodyFits(object, limit) {
+  let total = 0;
+  function loop(object) {
+    let keys = Object.keys(object);
+    for (let key in keys) {
+      if (typeof object[keys[key]] == "string") {
+        if (object.hasOwnProperty(keys[key])) total += object[keys[key]].length;
+      } else {
+        if (typeof object[keys[key]] == "object") {
+          loop(object[keys[key]]);
+        }
+      }
+    }
+  }
+  loop(object);
+  return total <= limit;
+}
 
 handler.post(async (req, res) => {
   const stacks = req.db.collection("stacks");
@@ -29,28 +56,46 @@ handler.post(async (req, res) => {
   //   iconLink: req.body.iconLink,
   //   time: new Date().toISOString()
   // };
-  if (req.body.link == undefined || validate(req.body.link, {
-    isUrl: true,
-    max: 100
-  })) {
-    req.body.link = scrubURL(req.body.link);
-    const body = {
-      ...req.body,
-      time: new Date().toISOString(),
-    };
+  if (req.body.link == undefined || isUrl(req.body.link)) {
+    if (fitsMax(req.body.name, 50) && fitsMax(req.body.tagline, 75) && bodyFits(req.body, 500)) {
+        if (req.body.name != undefined) {
+          req.body.link = scrubURL(req.body.link);
+          const body = {
+            name: req.body.name,
+          tagline: req.body.tagline,
+          langaugesAndFrameworks: req.body.langaugesAndFrameworks,
+          databaseAndStorage: req.body.databaseAndStorage,
+          technologies: req.body.technologies,
+          other: req.body.other,
+          deployment: req.body.deployment,
+          time: new Date().toISOString(),
+        };
 
-    stacks.insertOne(body, (err, response) => {
-      if (err) throw err;
-
-      res.status(200).send({
-        ok: true,
-        message: "Stack created sucessfully",
+        stacks.insertOne(body, (err, response) => {
+          if (err) throw err;
+          
+          res.status(200).send({
+            ok: true,
+            message: "Stack created sucessfully",
+          });
+        });
+      
+      } else {
+        res.status(400).json({
+          ok: false,
+          message: "No name received",
+        });
+      }
+    } else {
+      res.status(400).json({
+        ok: false,
+        message: "Max length exceded",
       });
-    });
+    }
   } else {
     res.status(400).json({
       ok: false,
-      message: "Invalid URL or too long url",
+      message: "Invalid URL",
     });
   }
 });
