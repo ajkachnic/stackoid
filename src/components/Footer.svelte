147f324b296100bc068bca970d66ff135636538b
Andrew Kachnic <ajkachnic@protonmail.com>
2020-04-30 10:25:18 -0400

    :bug: Updated footer and fixed styling
    I added the terms and privacy policy to the foote

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<style>
+	footer {
+		margin: 3em auto;
+    display: grid;
+    justify-content: center;
+    text-align: center;
+    color: var(--theme-foreground);
+	}
+  .links {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+  }
+</style>
+<footer>
+<p>Made with ❤️ by <a href="https://github.com/ajkachnic">Andrew Kachnic</a></p>
+<a href="https://github.com/ajkachnic/stackoid">We're open source🎉</a>
+<div class="links">
+  <a href="/terms#terms">Terms</a>
+  <a href="/terms#privacy">Privacy Policy</a>
+</div>
+</footer>
